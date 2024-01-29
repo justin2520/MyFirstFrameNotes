80ba42ddaef4460d05ddfe08c3eb946142eed504
justin.weber2520 <justin.weber25@d155.org>
2024-01-29 11:24:58 -0600

    GIT

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //1 Create a frame.
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+
+        //2 Create a panel.
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(100,50,180));
+        panel.setBackground(Color.CYAN);
+
+        //3 Create components.
+        JLabel label = new JLabel("Hello World!");
+        JButton button = new JButton("Don't Touch");
+        JTextField textField = new JTextField(5);
+        JScrollBar scrollBar = new JScrollBar();
+
+        // Add an image
+        ImageIcon icon = new ImageIcon("Images/PNG/2C.png");
+        icon.setImage(icon.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+        JLabel iconLabel = new JLabel(icon);
+
+        //4 Add components to the panel.
+        panel.add(label);
+        panel.add(button);
+        panel.add(textField);
+        panel.add(iconLabel);
+        panel.add(scrollBar);
+
+        //5 Add the panel to the frame.
+        frame.add(panel);
+
+        //6 Make the frame visible.
+        frame.setVisible(true);
+    }
+}
